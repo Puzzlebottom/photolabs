dffee79128e610bd4f1046f166f39954892eb7cc
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-07 20:49:29 -0400

    adds function to PhotoFaveButton

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,15 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+
 function PhotoFavButton() {
+  const [favourite, setFavourite] = useState(false);
+  const toggleFavourite = () => setFavourite(favourite ? false : true);
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={toggleFavourite}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={favourite} />
       </div>
     </div>
   );

9ee83d738f1ee5eb6faaa9ab0ad4775d643d7232
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-07 20:51:12 -0400

    composes PhotoList and TopNavBar into HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
+import HomeRoute from 'routes/HomeRoute';
 import './App.scss';
-
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const { id, location, imageSource, username, profile } = sampleDataForPhotoListItem;
+
   return (
     <div className="App">
-      <PhotoListItem key={id} location={location} imageSource={imageSource} username={username} profile={profile} />
+      <HomeRoute />
     </div>
   );
 };

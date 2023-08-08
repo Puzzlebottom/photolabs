7500b33842ed125cfde880df92a233edb0ba9a44
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-07 21:04:45 -0400

    replaces default props with imported mocks consumes through App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import HomeRoute from 'routes/HomeRoute';
+
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
+
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -7,7 +11,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photos={photos} topics={topics} />
     </div>
   );
 };
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,34 +3,16 @@ import TopicListItem from "./TopicListItem";
 
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const TopicList = (props) => {
 
-const TopicList = () => {
-
-  const topics = sampleDataForTopicList.map((topic) => {
+  const topicList = props.topics.map((topic) => {
     const { id, slug, title } = topic;
     return <TopicListItem key={id} slug={slug} title={title} />;
   });
 
   return (
     <div className="top-nav-bar__topic-list">
-      {topics}
+      {topicList}
     </div>
   );
 };

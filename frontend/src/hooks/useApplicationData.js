45636b3a8bac545dc2efb5b519c568b506b87238
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-09 10:26:31 -0400

    refactors useApplicationData from useState to useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,25 +1,53 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 
-const useApplicationData = () => {
-  const [favourites, setFavourites] = useState({});
-  const [selected, setSelected] = useState(null);
+const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  // SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  // SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+};
 
-  const state = { favourites, selected, photos, topics };
+const reducers = {
+  FAV_PHOTO_ADDED(state, action) {
+    const favourites = { ...state.favourites };
+    favourites[action.value] = true;
+    return { ...state, favourites };
+  },
+  FAV_PHOTO_REMOVED(state, action) {
+    const favourites = { ...state.favourites };
+    delete favourites[action.value];
+    return { ...state, favourites };
+  },
+  SELECT_PHOTO(state, action) {
+    const selected = action.value ? state.photos.find(photo => photo.id === action.value) : null;
+    return { ...state, selected };
+  }
+};
+
+const reducer = (state, action) => {
+  if (reducers[action.type]) {
+    return reducers[action.type](state, action);
+  }
+  return state;
+};
+
+const useApplicationData = () => {
+
+  const [state, dispatch] = useReducer(reducer, { favourites: {}, selected: null, photos, topics });
 
   const toggleFavourite = (id) => {
-    const currentFavourites = { ...favourites };
-    if (currentFavourites[id]) {
-      delete currentFavourites[id];
+    if (state.favourites[id]) {
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, value: id });
     } else {
-      currentFavourites[id] = true;
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, value: id });
     }
-    setFavourites(currentFavourites);
   };
 
-  const selectPhoto = (id) => setSelected(photos.find((photo) => photo.id === id));
-  const closeModal = () => setSelected(null);
+  const selectPhoto = (id) => dispatch({ type: ACTIONS.SELECT_PHOTO, value: id });
+  const closeModal = () => dispatch({ type: ACTIONS.SELECT_PHOTO, value: null });
 
   return {
     state,

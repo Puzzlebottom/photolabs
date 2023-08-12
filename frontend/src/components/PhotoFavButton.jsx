e2ab2f0fe6e2bf9a2a4b922d55715a3070f33619
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-11 22:34:46 -0400

    fixes bug with click event propagating through favButton and causing scroll in modal

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -5,8 +5,13 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton({ id, favourites, toggleFavourite }) {
 
+  const handleClick = (e) => {
+    e.stopPropagation();
+    toggleFavourite(id);
+  };
+
   return (
-    <div className="photo-list__fav-icon" onClick={() => toggleFavourite(id)}>
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
         <FavIcon selected={favourites[id]} />
       </div>
